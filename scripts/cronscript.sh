df5be64ee5f39c63a47c4dc0c689b17f8af40b0f
solresol <gregb@ifost.org.au>
2024-12-15 13:08:07 +1100

    Consolidate to one output file, and make sure that it is evaluation_runs that we're working with.

--- scripts/cronscript.sh
+++ scripts/cronscript.sh
@@ -5,22 +5,29 @@ cd ~/ultrametric-trees
 ./bin/evaluatemodel \
     -model /ultratree/language-model/tiny.sqlite \
     -model-cutoff-time "$(date +'%Y-%m-%d %H:%M:%S')" \
-    -test-data-database /ultratree/language-model/validation.sqlite \
+    -test-data-database /ultratree/language-model/testdata.sqlite \
     -run-description "Default daily $(date +%Y-%m-%d)" \
     -output-database ~/ultratree-results/inferences.sqlite
+
+for i in 2 3 4 5
+do
+    ./bin/evaluatemodel \
+	-model /ultratree/language-model/sense-annotated${i}.sqlite \
+	-model-cutoff-time "$(date +'%Y-%m-%d %H:%M:%S')" \
+	-test-data-database /ultratree/language-model/testdata.sqlite \
+	-run-description "Default daily $(date +%Y-%m-%d)" \
+	-output-database ~/ultratree-results/inferences.sqlite    
+done
 
 ./bin/evaluatemodel \
     -model /ultratree/language-model/unannotated-model1.sqlite \
     -model-cutoff-time "$(date +'%Y-%m-%d %H:%M:%S')" \
-    -test-data-database /ultratree/language-model/validation.sqlite \
+    -test-data-database /ultratree/language-model/testdata.sqlite \
     -run-description "Default daily for unnotated data model #1 $(date +%Y-%m-%d)" \
-    -output-database ~/ultratree-results/unannotated-model1.sqlite
+    -output-database ~/ultratree-results/inferences.sqlite
 
-sqlite3 ~/ultratree-results/inferences.sqlite ".dump validation_runs" > ~/ultratree-results/validation_runs.sql
+sqlite3 ~/ultratree-results/inferences.sqlite ".dump evaluation_runs" > ~/ultratree-results/evaluation_runs.sql
 sqlite3 ~/ultratree-results/inferences.sqlite ".dump inferences" > ~/ultratree-results/inferences.sql
-
-sqlite3 ~/ultratree-results/unannotated-model1.sqlite ".dump validation_runs" > ~/ultratree-results/unannotated-model1-validation_runs.sql
-sqlite3 ~/ultratree-results/unannotated-model1.sqlite ".dump inferences" > ~/ultratree-results/unannotated-model1-inferences.sql
 
 
 ./bin/report -db /ultratree/language-model/tiny.sqlite > ~/ultratree-results/training-results.csv
@@ -28,7 +35,7 @@ sqlite3 ~/ultratree-results/unannotated-model1.sqlite ".dump inferences" > ~/ult
 
 cd ~/ultratree-results
 git pull -q
-git add validation_runs.sql
+git add evaluation_runs.sql
 git add inferences.sql
 git add training-results.csv
 git add unannotated-model1-training-results.csv
