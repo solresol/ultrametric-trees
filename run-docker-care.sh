05820f815b00cf63bc07928ea699d3ec65a9f8dd
Greg Baker <greg.baker@anu.edu.au>
2024-12-28 07:57:21 +1100

    Add carefulness level charts.

---
+++ run-docker-care.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+docker rm careful10000-train1
+cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/careful10000.sqlite
+docker run --name=careful10000 \
+       --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
+       --database /ultratree/language-model/careful10000.sqlite \
+       --exemplar-guesses 10000 \
+       --cost-guesses 10000 \
+       --split-count-try 1000 \
+       --solar-monitor envoy.cassia.ifost.org.au --seed 1
+
+docker rm careful100-train1
+cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/careful100.sqlite
+docker run --name=careful100 \
+       --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
+       --database /ultratree/language-model/careful100.sqlite \
+       --exemplar-guesses 100 \
+       --cost-guesses 100 \
+       --split-count-try 10 \
+       --solar-monitor envoy.cassia.ifost.org.au --seed 1
+
+docker rm careful10-train1
+cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/careful10.sqlite
+docker run --name=careful10 \
+       --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
+       --database /ultratree/language-model/careful10.sqlite \
+       --exemplar-guesses 10 \
+       --cost-guesses 10 \
+       --split-count-try 10 \
+       --solar-monitor envoy.cassia.ifost.org.au --seed 1
+
--- scripts/cronscript.sh
+++ scripts/cronscript.sh
@@ -35,11 +35,21 @@ ENSEMBLE_MODEL=$(echo $ENSEMBLE_MODEL | sed 's/^@,//')
     -run-description "Default daily for unnotated data model #1 $(date +%Y-%m-%d)" \
     -output-database ~/ultratree-results/inferences.sqlite
 
+for CARE in 10 100 10000
+do
+  ./bin/evaluatemodel \
+    -model /ultratree/language-model/careful${CARE}.sqlite \
+    -model-cutoff-time "$(date +'%Y-%m-%d %H:%M:%S')" \
+    -test-data-database /ultratree/language-model/testdata.sqlite \
+    -run-description "Default daily for care ${CARE} data model $(date +%Y-%m-%d)" \
+    -output-database ~/ultratree-results/inferences.sqlite
+done 
+
 sqlite3 ~/ultratree-results/inferences.sqlite ".dump evaluation_runs" > ~/ultratree-results/evaluation_runs.sql
 sqlite3 ~/ultratree-results/inferences.sqlite ".dump inferences" > ~/ultratree-results/inferences.sql
 
 
-./bin/report -db /ultratree/language-model/sense-annotated${i}.sqlite > ~/ultratree-results/training-results.csv
+./bin/report -db /ultratree/language-model/sense-annotated1.sqlite > ~/ultratree-results/training-results.csv
 ./bin/report -db /ultratree/language-model/unannotated-model1.sqlite > ~/ultratree-results/unannotated-model1-training-results.csv
 
 cd ~/ultratree-results
