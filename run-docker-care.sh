4fd3c18806fa6c1d1446b87feb4ec3f5fffde2a3
Greg Baker <greg.baker@anu.edu.au>
2025-01-14 16:45:48 +1100

    Restart automatically.

--- run-docker-care.sh
+++ run-docker-care.sh
@@ -3,6 +3,7 @@
 docker rm careful10000-train1
 cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/careful10000.sqlite
 docker run --name=careful10000 \
+       --restart unless-stopped \
        --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
        --database /ultratree/language-model/careful10000.sqlite \
        --exemplar-guesses 10000 \
@@ -13,6 +14,7 @@ docker run --name=careful10000 \
 docker rm careful100-train1
 cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/careful100.sqlite
 docker run --name=careful100 \
+       --restart unless-stopped \
        --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
        --database /ultratree/language-model/careful100.sqlite \
        --exemplar-guesses 100 \
@@ -23,10 +25,10 @@ docker run --name=careful100 \
 docker rm careful10-train1
 cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/careful10.sqlite
 docker run --name=careful10 \
+       --restart unless-stopped \
        --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
        --database /ultratree/language-model/careful10.sqlite \
        --exemplar-guesses 10 \
        --cost-guesses 10 \
        --split-count-try 10 \
        --solar-monitor envoy.cassia.ifost.org.au --seed 1
-
--- run-docker-ensemble.sh
+++ run-docker-ensemble.sh
@@ -3,6 +3,7 @@
 docker rm sense-annotated-train1
 cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/sense-annotated1.sqlite
 docker run --name=sense-annotated-train1 \
+       --restart unless-stopped \
        --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
        --database /ultratree/language-model/sense-annotated1.sqlite \
        --solar-monitor envoy.cassia.ifost.org.au --seed 1
@@ -10,6 +11,7 @@ docker run --name=sense-annotated-train1 \
 docker rm sense-annotated-train2
 cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/sense-annotated2.sqlite
 docker run --name=sense-annotated-train2 \
+       --restart unless-stopped \
        --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
        --database /ultratree/language-model/sense-annotated2.sqlite \
        --solar-monitor envoy.cassia.ifost.org.au --seed 2
@@ -17,6 +19,7 @@ docker run --name=sense-annotated-train2 \
 docker rm sense-annotated-train3
 cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/sense-annotated3.sqlite
 docker run --name=sense-annotated-train3 \
+       --restart unless-stopped \
        --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
        --database /ultratree/language-model/sense-annotated3.sqlite \
        --solar-monitor envoy.cassia.ifost.org.au --seed 3
@@ -24,6 +27,7 @@ docker run --name=sense-annotated-train3 \
 docker rm sense-annotated-train4
 cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/sense-annotated4.sqlite
 docker run --name=sense-annotated-train4 \
+       --restart unless-stopped \
        --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
        --database /ultratree/language-model/sense-annotated4.sqlite \
        --solar-monitor envoy.cassia.ifost.org.au --seed 4
@@ -31,6 +35,7 @@ docker run --name=sense-annotated-train4 \
 docker rm sense-annotated-train5
 cp sense-annotated-training-dataframe.sqlite /ultratree/language-model/sense-annotated5.sqlite
 docker run --name=sense-annotated-train5 \
+       --restart unless-stopped \
        --detach=true -v /ultratree/language-model:/ultratree/language-model ultratree-train \
        --database /ultratree/language-model/sense-annotated5.sqlite \
        --solar-monitor envoy.cassia.ifost.org.au --seed 5
